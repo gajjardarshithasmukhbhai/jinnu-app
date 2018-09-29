df601a932aa8ff47397218c56ad53eec1111d00b
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-09-29 11:58:48 -1200

    jjdjdj
    jjj

--- src/nextpage.js
+++ src/nextpage.js
@@ -2,14 +2,56 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import  'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import './index.css'
+import './nextpage.css';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import {indigo} from '@material-ui/core/colors'
+import AddIcon from '@material-ui/icons/Add';
+const theme = createMuiTheme({
+	palatte:indigo[400],
+});
 
 class Nextpage extends React.Component{
 render()
 {
 	return(
 			<div>
-				<h1>gajjar</h1>	
+				<AppBar class="appbar">
+					<Toolbar >
+					</Toolbar>
+				</AppBar>
+				<br/>
+				<br/>
+				<div class="col-sm-12">
+					<Card>
+						 <CardContent>
+						 	<Grid continer alignItems="flex-end" style={{flex:1}}>
+						 		
+						        <Grid item class="text text-center email">
+						            <TextField  label="E-mail Adress" />
+						            <br/>
+						            <br/>
+						            <TextField  label="Jinu-Password" />
+						            <br/>
+						            <br/>
+						            <br/>
+						        	<Button>
+          								click me
+        							</Button>
+						        </Grid>
+						 	</Grid>
+						 </CardContent>
+					</Card>
+				</div>
 			</div>
 	);
 }
