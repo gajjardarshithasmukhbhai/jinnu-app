b0ae5ad9cc96c5fce500d8a9044893fd5347042a
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-09-30 04:31:48 -1200

    XXNN
    NN

--- src/nextpage.js
+++ src/nextpage.js
@@ -55,7 +55,7 @@ const appbar=style({
     backgroundColor: "#5535CC",
 })
 const usericon=style({
-    marginLeft:theme.spacing.unit * 12,
+    marginLeft:theme.spacing.unit * 13,
 })
 class Nextpage extends React.Component{
 render()
