1cc6fefcc971571684ac5a600e480439375ae6e1
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-10-14 14:10:08 -1200

    jjjwjwjj
    jjj

--- src/newspaper.js
+++ src/newspaper.js
@@ -35,6 +35,8 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import wer from './image/breaking.jpg'
 import red from '@material-ui/core/colors/red'
+import axios from 'axios'
+
 var firebase=require("firebase");
 var config = {
     apiKey: "AIzaSyAuyVZN2Sfzs_I-KFg8OekpJ0dHJ7Sd_H8",
@@ -93,24 +95,23 @@ class Newspaper extends React.Component{
 			photo:photoUrl,
 		})		
 	}
-	else
-	{
-
-	}
+	this.news();
 	}
 	constructor(props)
 	{
 		super(props);
 		this.state={
 			left:false,
-			photo:""
+			photo:"",
+      news:[],
 		};
     	this.toggleDrawer=this.toggleDrawer.bind(this);
     	this.back=this.back.bind(this);
     	this.backing=this.backing.bind(this);
     	this.logout=this.logout.bind(this);
     	this.logouting=this.logouting.bind(this);
-	}
+      this.news=this.news.bind(this);
+  }
 	back()
   {
   	this.setState({
@@ -142,6 +143,18 @@ class Newspaper extends React.Component{
       return <Redirect to="/next-page"/>
     }
   }
+  news()
+  {
+    axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=c9de7f6e3b714a7bb68f3f9320c58357`)
+    .then((result)=>
+    {
+      const post=result.data.articles;
+      this.setState({
+        news:post,
+      })
+    })
+  }
+
 render()
 {
 	const { classes } = this.props;
@@ -212,69 +225,48 @@ render()
 				<br/>
 				<br/>
 				
-				<div class="container">
-				<Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="Recipe" src={this.state.photo}className={classes.avatar}/>
-          }
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
-        />
-        <CardMedia
-          className={classes.media}
-          image={wer}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography component="p">
-            This impressive paella is a perfect party dish and a fun meal to cook together with your
-            guests. Add 1 cup of frozen peas along with the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <CardActions className={classes.actions} disableActionSpacing>
-          <IconButton aria-label="Add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="Share">
-            <ShareIcon />
-          </IconButton>
-          
-        </CardActions>
-        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-              minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-              heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-              browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-              chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-              salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-              minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-              without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
-              to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
-              cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
-              minutes more. (Discard any mussels that don’t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
-          </CardContent>
-        </Collapse>
-      </Card>
-      </div>
+        {
+          this.state.news.map((wr)=>{
+            return <div class="container">
+            <br/>
+            <Card className={classes.card}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="Recipe" src={this.state.photo}className={classes.avatar}/>
+              }
+              action={
+                <IconButton>
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={wr.title}
+              subheader={wr.publishedAt}
+            />
+            <CardMedia
+              className={classes.media}
+              image={wr.urlToImage}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography component="p">
+                {wr.description}
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.actions} disableActionSpacing>
+              <IconButton aria-label="Add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="Share">
+                <ShareIcon />
+              </IconButton>
+              
+            </CardActions>
+            
+          </Card>
+      </div> 
+          })
+        }
+				
 			</div>
 			</div>
 	);
